709a86f64441b74ca854257c699dba61e5ce8ae5
LimHanGyeol <hang19663@gmail.com>
2021-09-18 15:30:45 +0900

    Initial First Service

---
+++ first-service/src/test/java/com/tommy/firstservice/FirstServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tommy.firstservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
