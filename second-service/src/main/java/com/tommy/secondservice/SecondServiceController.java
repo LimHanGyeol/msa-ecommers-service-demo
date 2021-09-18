1e1411a73318db3de71865bd2360157cb6584a2a
LimHanGyeol <hang19663@gmail.com>
2021-09-18 17:06:22 +0900

    [Spring Cloud Gateway] API-Gateway 구현
    Spring Cloud Gateway를 이용하여 API Gateway를 구현한다.
    Filter 및 Load Balancer 기능을 구현한다.
    
    work items: #4
    
    [Spring Cloud Gateway] - Filter 적용(Property)
    
    Property.yml로 Routing 설정을 한다.
    Source Code Config로 이용하기 위해 yml 설정을 주석 처리한다.
    
    work items: #4
    
    [Spring Cloud Gateway] - Custom Filter 적용
    
    work items: #4
    
    [Spring Cloud Gateway] - Global Filter 적용
    
    work items: #4
    
    [Spring Cloud Gateway] - Logging Filter 적용
    
    work items: #4
    
    [Spring Cloud Gateway] - Load Balancer
    
    work items: #4

--- second-service/src/main/java/com/tommy/secondservice/SecondServiceController.java
+++ second-service/src/main/java/com/tommy/secondservice/SecondServiceController.java
@@ -1,9 +1,12 @@
 package com.tommy.secondservice;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/second-service")
 public class SecondServiceController {
@@ -11,5 +14,16 @@ public class SecondServiceController {
     @GetMapping("/welcome")
     public String welcome() {
         return "Welcome to the Second service!";
+    }
+
+    @GetMapping("/message")
+    public String message(@RequestHeader("second-request") String header) {
+        log.info(header);
+        return "Hello World in Second Service.";
+    }
+
+    @GetMapping("/check")
+    public String check() {
+        return "Hi, there This is message from Second Service.";
     }
 }
