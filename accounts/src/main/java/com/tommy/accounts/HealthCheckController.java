20830977e2ec73d26ea051df9a57d69832f93ba4
LimHanGyeol <hang19663@gmail.com>
2021-09-25 18:01:23 +0900

    [Accounts Microservice] API Gateway에 Accounts 등록
    * API Gateway에 Accuonts Microservice를 등록한다.
    * 등록간 HealthCheck RequestMapping의 URI Path를 변경한다.
    
    work items: #8

--- accounts/src/main/java/com/tommy/accounts/HealthCheckController.java
+++ accounts/src/main/java/com/tommy/accounts/HealthCheckController.java
@@ -2,20 +2,23 @@ package com.tommy.accounts;
 
 import com.tommy.accounts.vo.Greeting;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/")
+@RequestMapping("/accounts-service")
 public class HealthCheckController {
 
+    private final Environment environment;
     private final Greeting greeting;
 
     @GetMapping("/health_check")
     public String healthCheck() {
-        return "It's Working in Accounts Service";
+        String port = environment.getProperty("local.server.port");
+        return String.format("It's Working in Accounts Service on PORT : %s", port);
     }
 
     @GetMapping("/welcome")
