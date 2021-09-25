33dd9aa3008b94ecb06f2c300c2010d43b78ab40
LimHanGyeol <hang19663@gmail.com>
2021-09-26 00:26:07 +0900

    Initial Catalogs Service

---
+++ catalogs/src/test/java/com/tommy/catalogs/CatalogsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tommy.catalogs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
